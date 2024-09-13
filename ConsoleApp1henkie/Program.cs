f9be1903282809565bf135004c56ca55882f07a4
Martijn <oldmartijntje@gmail.com>
2024-09-13 15:28:52 +0200

    idk, init or something. go figure

---
+++ ConsoleApp1henkie/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+// See https://aka.ms/new-console-template for more information
+string userName = Environment.UserName;
+
+// Get the IP address
+string localIP = GetLocalIPAddress();
+
+// Output to console
+Console.WriteLine($"hellu {userName}, {localIP} is your IP");
+Console.ReadLine();
+
+static string GetLocalIPAddress()
+{
+    string localIP = string.Empty;
+    foreach (var ip in Dns.GetHostAddresses(Dns.GetHostName()))
+    {
+        if (ip.AddressFamily == AddressFamily.InterNetwork)
+        {
+            localIP = ip.ToString();
+            break;
+        }
+    }
+    return localIP;
+}
