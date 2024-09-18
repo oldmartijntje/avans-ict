9073dd8f3958bd696cc6f7b40e5d7250834d7e8e
Martijn <oldmartijntje@gmail.com>
2024-09-18 15:15:16 +0200

    09_18_24

--- year1/period1/applications/09_13_24a-ConsoleApp1henkie/obj/Debug/net8.0/ConsoleApp1henkie.AssemblyInfo.cs
+++ year1/period1/applications/09_13_24a-ConsoleApp1henkie/obj/Debug/net8.0/ConsoleApp1henkie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1henkie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf92a56bac4cacc908fc9d5271c9e800a18592cf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+489b67aefc2ec7007b111b0a34f27469a929b6c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1henkie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1henkie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
